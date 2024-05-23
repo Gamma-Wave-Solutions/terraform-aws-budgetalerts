c9df2416cab7d5d63ef3177ff492ce55510193b0
Scott Evers <scottaevers+github@gmail.com>
2024-05-23 18:07:12 -0500

    removing provider

--- variables.tf
+++ variables.tf
@@ -1,25 +1,3 @@
-variable "AWS_DEFAULT_REGION" {
-  type = string
-}
-
-variable "AWS_ACCESS_KEY_ID" {
-  type = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
-}
-
-variable "AWS_ACCOUNT_ID" {
-  type = string
-  default = ""
-}
-
-variable "AWS_CROSS_ACCOUNT_ROLE" {
-  type = string
-  default = "OrganizationAccountAccessRole"
-}
-
 variable "notify_email" {
   type = string
 }
